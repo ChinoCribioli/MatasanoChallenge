61d1166be83edf2a843480459d87d46c6cc6f56a
ChinoCribioli <54038430+ChinoCribioli@users.noreply.github.com>
2023-12-08 17:10:31 -0300

    Add files via upload

--- Set1.py
+++ Set1.py
@@ -84,7 +84,6 @@ def challenge2_fixed_XOR(hash1,hash2):
 	bits1 = hex_to_bits(hash1)
 	bits2 = hex_to_bits(hash2)
 	finalBits = [bits1[i]^bits2[i] for i in range(len(bits1))]
-
 	return bits_to_hex(finalBits)
 
 print("Set 1, Challenge 2:")
@@ -113,6 +112,9 @@ def apply_repeatedXor(bits, key): # key must be an array of bits
 	for i in range(len(bits)):
 		response[i] = bits[i] ^ key[i%keyLength]
 	return response
+
+def is_legal_ascii(index):
+	return index <= 126 and not (index in range(12,32))
 
 letterFrequency = {
 	'E' : 12.0,
@@ -156,6 +158,8 @@ def letter_frequency_metric(bits):
 	assert(l%8 == 0)
 	for i in range(0,l//8):
 		index = bits_to_int(bits[8*i:8*(i+1)])
+		if not is_legal_ascii(index):
+			return 1000*l
 		if index in range(oa,oz+1):
 			appearances[chr(index-oa+oA)] += 1/l
 		elif index in range(oA,oZ+1):
@@ -168,20 +172,24 @@ def letter_frequency_metric(bits):
 		else:
 			penalty += 20/l # I penalize every character that is neither a letter, a number or a space
 	for key, value in letterFrequency.items():
-		penalty += (value-appearances[key])**2
+		penalty += abs(value-appearances[key])#**2
 	return penalty
 
-def challenge3_guess_xor_key(hash):
-	hashBits = hex_to_bits(hash)
+def guess_xor_key(hashBits):
 	bestPenalty = 10000000000000
-	bestKey = ''
+	bestKey = []
 	for i in range(256):
-		guess = apply_repeatedXor(hashBits,int_to_bits(i))
+		key = int_to_bits(i)
+		guess = apply_repeatedXor(hashBits,key)
 		guessPenalty = letter_frequency_metric(guess)
 		if guessPenalty < bestPenalty:
 			bestPenalty = guessPenalty
-			bestKey = i
-	return (bestKey, bits_to_ascii(apply_repeatedXor(hashBits,int_to_bits(bestKey))))
+			bestKey = key
+	return (bestKey, apply_repeatedXor(hashBits,bestKey))
+
+def challenge3_guess_xor_key(hash):
+	guess = guess_xor_key(hex_to_bits(hash))
+	return (bits_to_int(guess[0]),bits_to_ascii(guess[1]))
 
 
 print("Set 1, Challenge 3:")
@@ -204,29 +212,22 @@ def challenge4_find_message():
 	bestIndex = -1
 	bestKey = ''
 	for i in range(len(sentences)):
-		guess = challenge3_guess_xor_key(sentences[i])
-		penalty = letter_frequency_metric(string_to_bits(guess[1]))
+		sentenceBits = hex_to_bits(sentences[i])
+		guess = guess_xor_key(sentenceBits)
+		penalty = letter_frequency_metric(guess[1])
 		if penalty < bestPenalty:
 			bestPenalty = penalty
 			bestIndex = i
 			bestKey = guess[0]
-	return (bestIndex, bestKey, bits_to_ascii(apply_repeatedXor(hex_to_bits(sentences[bestIndex]),int_to_bits(bestKey))))
+	return (bestIndex, bits_to_int(bestKey), bits_to_ascii(apply_repeatedXor(hex_to_bits(sentences[bestIndex]),bestKey)))
 	
 print("Set 1, Challenge 4:")
 print("ok" if challenge4_find_message() == (170, 53, 'Now that the party is jumping\n') else "--------------FAILED--------------")
 
 ############################################################################################
 
-def apply_repeatingKey_xor_to_string(message,key):
-	l = len(key)
-	newMessage = ""
-	for i in range(len(message)):
-		newMessage += chr(ord(message[i])^ord(key[i%l]))
-	return newMessage
-	
 def challenge5_encrypt_with_repeating_key_xor(message,key):
-	return bits_to_hex(string_to_bits(apply_repeatingKey_xor_to_string(message,key)))
-
+	return bits_to_hex(apply_repeatedXor(string_to_bits(message),string_to_bits(key)))
 
 print("Set 1, Challenge 5:")
 print("ok" if challenge5_encrypt_with_repeating_key_xor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal","ICE")
@@ -259,6 +260,14 @@ def base64_to_bits(string):
 			break
 		assert(c == bits_to_base64(base64Char_to_bits(c)))
 		bits += base64Char_to_bits(c)
+	if string[-2:len(string)] == "==":
+		for i in range(1,5):
+			assert(bits[-i] == 0)
+		bits = bits[0:-4]
+	elif string[-1:len(string)] == "=":
+		for i in range(1,3):
+			assert(bits[-i] == 0)
+		bits = bits[0:-2]
 	return bits
 
 def hamming_distance_bits(bits1,bits2):
@@ -272,44 +281,77 @@ def hamming_distance_bits(bits1,bits2):
 def hamming_distance_strings(string1,string2):
 	return hamming_distance_bits(string_to_bits(string1),string_to_bits(string2))
 
+assert(hamming_distance_strings("this is a test","wokka wokka!!!") == 37)
+
+def find_best_keysize(message):
+	bestKeysize = -1
+	bestHammingDistance = 1000000000000000
+	for keysize in range(1,41): # tiene sentido probar con 1?
+		currentDistance = 0
+		# len(message)//8 is the number of bytes, len(message)//(8*keysize) is the number of disjoint blocks of keysize bytes
+		for i in range(1,len(message)//(8*keysize)):
+			currentDistance += hamming_distance_bits(message[(i-1)*8*keysize:(i)*8*keysize],message[(i)*8*keysize:(i+1)*8*keysize])/keysize
+		currentDistance /= len(message)//(8*keysize)-1 # Since we calculated several normalized-hamming-distances, we want to average them since the numeber of comparisons vary between different keysizes
+		if currentDistance < bestHammingDistance:
+			bestHammingDistance = currentDistance
+			bestKeysize = keysize
+	return bestKeysize
+	# This is a way of finding the keysize that is not very effective but a bit curious. It states that the keysize is equal to the gcd of the all the differences of position of equal bytes.
+	# The logic behind this is that if we have two equal bytes, it its probable that they are equal because they were the same letter before the encryption AND they are in positions with the same remainder modulo keysize.
+	# This is not very reliable when the message is long because having a colission of type A^B = C^D breaks the logic
+	'''
+	import math
+	positions = [[] for _ in range(256)]
+	for i in range(len(message)//8):
+		n = bits_to_int(message[8*i:8*(i+1)])
+		positions[n].append(i)
+	keysize = 0
+	for n in range(256):
+		if len(positions[n]) < 2:
+			continue
+		differences = [positions[n][i]-positions[n][i-1] for i in range(1,len(positions[n]))]
+		for dif in differences:
+			keysize = math.gcd(keysize,dif)
+	return keysize
+	'''
+
+def guess_xor_key_by_space(message):
+	# This technique tries to find the key of a message by assuming that the most common character in a message is a space
+	counter = [0 for i in range(256)]
+	for i in range(len(message)//8):
+		counter[bits_to_int(message[8*i:8*(i+1)])] += 1
+	moreCommonChar = -1
+	appearances = -1
+	for i in range(256):
+		if appearances < counter[i]:
+			appearances = counter[i]
+			moreCommonChar = i
+	key = int_to_bits(moreCommonChar ^ ord(' '))
+	return (key,apply_repeatedXor(message,key))
+
+def bytes_to_ints(bits):
+	assert(len(bits)%8 == 0)
+	return [bits_to_int(bits[8*i:8*(i+1)]) for i in range(len(bits)//8)]
+
 def challenge6_break_repeating_key_xor():
 	with open('s1c6') as f:
 	    stringMessage = f.read().replace('\n',"")
 	message = base64_to_bits(stringMessage)
-	if stringMessage[-2:len(stringMessage)] == "==":
-		for i in range(1,5):
-			assert(message[-i] == 0)
-		message = message[0:-4]
-	elif stringMessage[-1:len(stringMessage)] == "=":
-		for i in range(1,3):
-			assert(message[-i] == 0)
-		message = message[0:-2]
 	assert(len(message)%8 == 0)
-	bestKeysize = -1
-	bestHammingDistance = 1000000000000000
-	for keysize in range(1,41): # tiene sentido probar con 1?
-		currentDistance = hamming_distance_bits(message[0:8*keysize],message[8*keysize:16*keysize])/keysize
-		# print("keysize:",keysize,"is",currentDistance)
-		if currentDistance < bestHammingDistance:
-			bestHammingDistance = currentDistance
-			bestKeysize = keysize
-	blocks = [[] for i in range(bestKeysize)]
+	keysize = find_best_keysize(message)
+	blocks = [[] for i in range(keysize)]
 	for i in range(len(message)//8):
-		blocks[i%bestKeysize] += message[8*i:8*(i+1)]
-	encryptionKey = ""
-	for i in range(bestKeysize):
-		print(len(blocks[i]))
-		print(len(bits_to_ascii(blocks[i]))%8)
-		print(len(bits_to_ascii(blocks[i])))
-		decryption = challenge3_guess_xor_key(bits_to_ascii(blocks[i]))
-		# print(bits_to_ascii(blocks[i]))
-		print(decryption)
-		encryptionKey += chr(decryption[0])
-	print("the encryptionKey is:", encryptionKey)
-	return apply_repeatingKey_xor_to_string(stringMessage,encryptionKey)
-
-assert(hamming_distance_strings("this is a test","wokka wokka!!!") == 37)
+		blocks[i%keysize] += message[8*i:8*(i+1)]
+	encryptionKey = []
+	for i in range(keysize):
+		copy = blocks[i]
+		# decryption = guess_xor_key(blocks[i])
+		decryption = guess_xor_key_by_space(blocks[i])
+		assert(copy == blocks[i])
+		encryptionKey += decryption[0]
+	# print("the encryptionKey is:", bits_to_ascii(encryptionKey), f"({len(bits_to_ascii(encryptionKey))})")
+	return bits_to_ascii(apply_repeatedXor(message,encryptionKey))
 
 print("Set 1, Challenge 6:")
-print("the result of the challenge is:", challenge6_break_repeating_key_xor()[0:100])
-# challenge6_break_repeating_key_xor()
+print("the result of the challenge is:")
+print(challenge6_break_repeating_key_xor())
